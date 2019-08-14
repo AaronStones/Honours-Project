5386d672723a525c7ba34759b6a6b459c845d64f
Aaron Stones <aaron@escapo.co.uk>
2019-08-14 17:05:14 +0100

    navigation completed

---
+++ View/contactus.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("templates/header.php"); ?>
+</head>
+    <body>
+        <?php include("templates/nav.php"); ?>
+
+
+        <?php include("templates/footer.php"); ?>
+    </body>
+</html>
--- View/templates/nav.php
+++ View/templates/nav.php
@@ -1,22 +1,24 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/index">Elderly Helper</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Help<span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
+          <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/account">Account</a></li>
+          <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/aboutus">About Us</a></li>
+          <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/faqs">FAQs</a></li>
+          <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/contactus">Contact us</a></li>
+
         </ul>
       </li>
-      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Main Application Content</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/signup"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
   </div>
 </nav>
