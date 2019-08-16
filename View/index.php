a634a237edd57308f8edc4e919dbdfcd46f50914
Aaron Stones <aaron@escapo.co.uk>
2019-08-16 13:51:30 +0100

    ensured a header was added to the code

--- View/index.php
+++ View/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/css/index.css">
     <?php include("templates/header.php"); ?>
 </head>
     <body>
         <?php include("templates/nav.php"); ?>
 
+            <div class="header">
+                <a href="https://mayar.abertay.ac.uk/~1600964/Honours-Project/View/index"><h1>Honours Project</h1></a>
+                <h3>Aaron Stones | BSc Computing | Abertay University</h3>
+            </div>
 
         <?php include("templates/footer.php"); ?>
     </body>
