8d524560c6fea2c01101912b738f65a9377d5024
AaronStones <43172316+AaronStones@users.noreply.github.com>
2019-08-29 18:52:09 +0100

    git commit

--- Controller/login.php
+++ Controller/login.php
@@ -1,8 +1,8 @@
 <?php
-    //header('Content-Type', 'application/json');
-    //include_once("../Model/user.php");
+    header('Content-Type', 'application/json');
+    include_once("../Model/user.php");
     echo $_POST['email'];
-    /*$user = Login($_POST['email'], $_POST['password']); //Login function
+    $user = Login($_POST['email'], $_POST['password']); //Login function
 
     if ($user != null) //If User is logged in
     {
@@ -12,14 +12,5 @@
     else //if User is not logged in
     {
         echo json_encode(array('success' => false, 'message' => 'Incorrect email or password.'));
-    }*/
-
-
-    function debug_to_console($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
-    
-        echo "<script>alert('Debug Objects: " . $output . "' );</script>";
     }
 ?>
