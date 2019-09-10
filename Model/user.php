2558ab08ebe36cf0a7d93ca7e73b51a88502731c
Aaron Stones <aaron@escapo.co.uk>
2019-09-10 13:50:15 +0100

    login form testing

--- Model/user.php
+++ Model/user.php
@@ -9,7 +9,7 @@ function Login($email, $pword){
 
     global $conn;
 
-    $sql = "SELECT * FROM Honours.JSON WHERE email ='stones@gmail.com'";
+    $sql = "SELECT * FROM Honours.JSON WHERE email =" . $email;
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -38,12 +38,17 @@ function Login($email, $pword){
         return null;
     }
     while($row = $result->fetch_assoc()) {
-        $ids[] = $row['id'];
-        $names[] = $row['name'];
-        $ages[] = $row['age'];
-    }
-    var_export($ages);
-    $stmt->close();*/
+        if (!password_verify($pword, $row['password']))
+            {
+                return null; // Password does not match hash
+            }
+            else {
+                return json_encode(array(
+                    'id' => $row['Primary_Key'],
+                    'email' => $row['email'],
+                    'admin' => $row['JSON']));
+                }
+    }*/
 
 }
 
