ca59876e83347b9d869fe1f80db66e7cae320803
Aaron Stones <aaron@escapo.co.uk>
2019-09-13 09:29:11 +0100

    login test

--- Model/user.php
+++ Model/user.php
@@ -2,54 +2,37 @@
 include("connection.php");
 $db = new dbObj();
 $conn =  $db->getConnstring();
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 
-
-function Login($email, $pword){
+//function Login($email, $pword){
     
-
+    $email = "stones@gmail.com";
     global $conn;
 
-    $sql = "SELECT * FROM Honours.JSON WHERE email =" . $email;
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            if (!password_verify($pword, $row['password']))
-            {
-                return null; // Password does not match hash
-            }
-            else {
-                return json_encode(array(
-                    'id' => $row['Primary_Key'],
-                    'email' => $row['email'],
-                    'admin' => $row['JSON']));
-                }
-        }
-    } 
-
-
-    /*$stmt = $mysqli->prepare("SELECT * FROM Honours.JSON WHERE email ='?'");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result->num_rows === 0) 
+		
+    $statement = $conn->prepare('SELECT Primary_Key, email, password, JSON FROM Honours.JSON WHERE email = :email');
+    $statement->bindParam(':email', $email);
+    $statement->execute();
+    
+    /*if ($statement->rowCount() == 0)
     {
-        return null;
+        return null; // Email does not exist
     }
-    while($row = $result->fetch_assoc()) {
-        if (!password_verify($pword, $row['password']))
-            {
-                return null; // Password does not match hash
-            }
-            else {
-                return json_encode(array(
-                    'id' => $row['Primary_Key'],
-                    'email' => $row['email'],
-                    'admin' => $row['JSON']));
-                }
-    }*/
+    
+    $row = $statement->fetch();
+    
+    if (!password_verify($pword, $row['password']))
+    {
+        return null; // Password does not match hash
+    }
 
-}
+    // Everything in order, return user data as JSON
+    return json_encode(array(
+        'id' => $row['Primary_Key'],
+        'email' => $row['email'],
+        'admin' => $row['JSON']));*/
+
+//}
 
 ?>
