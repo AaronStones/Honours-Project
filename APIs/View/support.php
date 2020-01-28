7e4ddbb5b067f1a83c5c3ccd0c0101499642f5fa
Aaron <1600964@uad.ac.uk>
2020-01-28 17:00:26 +0000

    updated support page

--- APIs/View/support.php
+++ APIs/View/support.php
@@ -6,7 +6,6 @@
 </head>
     <body>
         <?php include("templates/nav.php"); ?>
-        <h1>Support</h1>
 <!-- Start of page content -->
 
 <div class="container my-3" role="main">
@@ -14,34 +13,34 @@
         <div class="col-md-6 col-sm-12">
             <h2>Frequently Asked Questions</h2>
 
-            <h6>How will my data be stored?</h6>
+            <h3>How will my data be stored?</h3>
             <p class="small">
             Your data will be securely stored in the university's MySQL database on the 
             Mayar server.
             </p>
 
-            <h6>Does this application ue 'real life' data/results?</h6>
+            <h3>Does this application ue 'real life' data/results?</h3>
             <p class="small">
             No all data/reuslts are fabricated or taken from willing volunteersto prove the 
             concept works. No patient data is ever used. 
             </p>
 
-            <h6>Where is this project being developed?</h6>
+            <h3>Where is this project being developed?</h3>
             <p class="small">
             This project is being developed within the Univeristy of Abertay Dundee's campus.
             </p>
 
-            <h6>What technologies does the project utilise?</h6>
+            <h3>What technologies does the project utilise?</h3>
             <p class="small">
             To develop this website the technologies used are the LAMP (or Linux Apache MySQL and PHP)
             stack with HTML, CSS and JavaScript to suppliment the website. 
             </p>
-            <h6>What else is being developed to couple with this website?</h6>
+            <h3>What else is being developed to couple with this website?</h3>
             <p class="small">
             A mobile device to test patients Parkinson's/Dementia state and an IoT device to read a subject's
             vital signs. 
             </p>
-            <h6>My question isn't answered here what should I do?</h6>
+            <h3>My question isn't answered here what should I do?</h3>
             <p class="small">
             Please fill out the form on this page and it will send an email to a member of the development team
             who will answer your query in no time.
@@ -61,7 +60,7 @@
                 <input class="form-control" type="email" name="email" required pattern="^(?=.{5,80}$)[\S]+@[\S]+\.[\S]+$">
                 <label for="message">Message</label>
                 <textarea class="form-control" name="message" rows="6" required pattern="^.{1,1200}$"></textarea>
-                <input class="form-control btn btn-danger mt-3" type="submit" value="Submit">
+                <input class="form-control btn btn-success mt-3" type="submit" value="Submit">
             </form>
         </div>
     </div>
