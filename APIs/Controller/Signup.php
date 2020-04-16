5dd33924855e0733f344d5712a88260ad9b8be87
Aaron Stones <aaron@escapo.co.uk>
2020-04-16 20:14:05 +0100

    this change is a test

--- APIs/Controller/Signup.php
+++ APIs/Controller/Signup.php
@@ -3,7 +3,7 @@
 //Author: Aaron Stones
 //Date: 10/04/2020
 //Purpose: to show the benefits of collecting lots of data about a patient
-//using different devices and tests
+//using different devices and tests of the different patients
 
 include("../Model/User.php");
 
